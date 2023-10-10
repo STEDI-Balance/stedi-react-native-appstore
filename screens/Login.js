99f690be270e37f11847dc14f1ec34432cbadf96
mustardowl <arianne.dilzer@gmail.com>
2023-10-10 14:06:50 -0400

    Arianne Button

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, Alert} from 'react-native';
 
 export default function Login(props){
     return(
         <View style={styles.login}>
             <Text>This is the Login Screen</Text>
-            <Button title="Log In" onPress={()=>{}}></Button>
+            <Button title="Log In" onPress={()=>{Alert.alert("Hello Reload")}}></Button>
         </View>
 
     );
